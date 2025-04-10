0c789c8f0c8da17fc907ff2480eb9edf3e019493
ngnff <99824356+ngnff@users.noreply.github.com>
2025-04-10 09:21:00 +0400

    Update task01.ino

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,26 @@
-void setup() {
-
+void setup () {
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
-void loop() {
+unsigned long timerD3, timerD5, timerD6, timerD9, timerD10;
+bool d3, d5, d6, d9, d10;
 
+#define set_strobe(now, pin, period)         \
+  if (now - timerD##pin >= period / 2) {     \
+    digitalWrite(pin, d##pin ? HIGH : LOW);  \
+    d##pin = !d##pin;                        \
+    timerD##pin = now;                       \
+  }
+
+void loop () {
+  unsigned long now = micros();
+  set_strobe(now, 10, 50);
+  set_strobe(now, 9, 100);
+  set_strobe(now, 6, 500);
+  set_strobe(now, 5, 1000);
+  set_strobe(now, 3, 10000);
 }
